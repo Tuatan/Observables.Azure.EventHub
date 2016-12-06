8e927f2bfc27f02a10575bfc90ba40059b02c7b0
Sergey Baranchenkov <tuatan@yandex.ru>
2016-12-05 19:38:34 -0800

    Initial release

---
+++ EventHubListener.cs
@@ -0,0 +1,117 @@
+﻿namespace Ecs.Azure
+{
+    using System;
+    using System.Linq;
+    using System.Reactive.Disposables;
+    using System.Reactive.Linq;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    using Microsoft.ServiceBus.Messaging;
+
+    /// <summary>
+    /// This type that exposes IoT Event Hub instance as an observable source.
+    /// </summary>
+    public sealed class EventHubListener : IObservable<EventData>
+    {
+        private readonly string eventHubName;
+
+        private readonly string eventHubConnectionString;
+
+        private readonly string consumerGroupName;
+
+        private readonly TimeSpan period;
+
+        private EventHubConsumerGroup consumerGroup;
+
+        private readonly IObservable<EventData> source;
+
+        /// <summary>
+        /// Creates a new instance of the EventHubsListener using the specified connection string.
+        /// </summary>
+        /// 
+        /// <returns>
+        /// The newly created <see cref="T:Ecs.EventHubs.EventHubsListener"/> object.
+        /// </returns>
+        /// <param name="eventHubName">The path to the Event Hub from which to start receiving event data.</param>   
+        /// <param name="eventHubConnectionString">The connection string for the Event Hub.</param>
+        /// <param name="consumerGroupName">The consumer group name.</param>
+        /// <param name="pollingPeriod">Polling interval.</param>
+        public EventHubListener(
+            string eventHubName,
+            string eventHubConnectionString,
+            string consumerGroupName,
+            TimeSpan pollingPeriod)
+        {
+            if (eventHubName == null)
+            {
+                throw new ArgumentNullException(nameof(eventHubName));
+            }
+            if (eventHubConnectionString == null)
+            {
+                throw new ArgumentNullException(nameof(eventHubConnectionString));
+            }
+
+            this.eventHubName = eventHubName;
+            this.eventHubConnectionString = eventHubConnectionString;
+            this.consumerGroupName = consumerGroupName;
+            this.period = pollingPeriod;
+
+            this.source = Observable.Create((Func<IObserver<EventData>, CancellationToken, Task<IDisposable>>)this.CreateStream)
+               .Publish()
+               .RefCount();
+        }
+
+        /// <summary>
+        /// Notifies the provider that an observer is to receive notifications.
+        /// </summary>
+        /// 
+        /// <returns>
+        /// A reference to an interface that allows observers to stop receiving notifications before the provider has finished sending them.
+        /// </returns>
+        /// <param name="observer">The object that is to receive notifications.</param>
+        public IDisposable Subscribe(IObserver<EventData> observer)
+        {
+            if (observer == null)
+            {
+                throw new ArgumentNullException(nameof(observer));
+            }
+
+            return this.source.Subscribe(observer);
+        }
+
+        private async Task<IDisposable> CreateStream(IObserver<EventData> observer, CancellationToken cancellationToken)
+        {
+            var eventHubClient = EventHubClient.CreateFromConnectionString(
+                this.eventHubConnectionString,
+                this.eventHubName);
+
+            this.consumerGroup = string.IsNullOrEmpty(this.consumerGroupName)
+                ? eventHubClient.GetDefaultConsumerGroup()
+                : eventHubClient.GetConsumerGroup(this.consumerGroupName);
+
+            var receiver = await this.consumerGroup.CreateReceiverAsync("7").ConfigureAwait(false);
+
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                var items = await receiver.ReceiveAsync(10).ConfigureAwait(false);
+
+                var eventDatas = items as EventData[] ?? items.ToArray();
+
+                if (eventDatas.Length != 0)
+                {
+                    foreach (var item in eventDatas)
+                    {
+                        observer.OnNext(item);
+                    }
+                }
+                else
+                {
+                    await Task.Delay(this.period, cancellationToken).ConfigureAwait(false);
+                }
+            }
+
+            return Disposable.Create(receiver.Close);
+        }
+    }
+}
---
+++ EventHubPublisher.cs
@@ -0,0 +1,110 @@
+﻿namespace Ecs.Azure
+{
+    using System;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    using Microsoft.ServiceBus.Messaging;
+
+    /// <summary>
+    /// Exposes Event Hub publications as a sink.
+    /// </summary>
+    public sealed class EventHubPublisher : IObserver<EventData>, IDisposable
+    {
+        private EventHubClient client;
+
+        /// <summary>
+        /// Creates a new instance of the EventHubsPublisher.
+        /// </summary>
+        /// <param name="eventHubConnectionString">The connection string to be used.</param>
+        /// <param name="path">The path to the Event Hub.</param>
+        /// <exception cref="ArgumentNullException">Thrown if any of the parameters are null.</exception>
+        public EventHubPublisher(
+            string eventHubConnectionString, 
+            string path)
+        {
+            if (string.IsNullOrWhiteSpace(eventHubConnectionString))
+            {
+                throw new ArgumentNullException(nameof(eventHubConnectionString));
+            }
+
+            if (string.IsNullOrWhiteSpace(path))
+            {
+                throw new ArgumentNullException(nameof(path));
+            }
+
+            this.client = EventHubClient.CreateFromConnectionString(eventHubConnectionString, path);
+        }
+
+        /// <summary>
+        /// Provides the observer with new data.
+        /// </summary>
+        /// <param name="value">The current notification information.</param>
+        public void OnNext(EventData value)
+        {
+            this.client.Send(value);
+        }
+
+        /// <summary>
+        /// Notifies the observer that the provider has experienced an error condition.
+        /// </summary>
+        /// <param name="error">An object that provides additional information about the error.</param>
+        public void OnError(Exception error)
+        {
+            this.Dispose();
+        }
+
+        /// <summary>
+        /// Notifies the observer that the provider has finished sending push-based notifications.
+        /// </summary>
+        public void OnCompleted()
+        {
+            this.Dispose();
+        }
+
+        /// <summary>
+        /// Provides the observer with new data.
+        /// </summary>
+        /// <param name="value">
+        /// The newly produced data.
+        /// </param>
+        /// <returns>
+        /// A Task that completes when the observer has finished processing <paramref name="value"/>.
+        /// </returns>
+        public Task OnNextAsync(EventData value)
+        {
+            return this.client.SendAsync(value);
+        }
+
+        public Task OnErrorAsync(Exception error)
+        {
+            this.Dispose();
+            return Task.FromResult(1);
+        }
+
+        public Task OnCompletedAsync()
+        {
+            this.Dispose();
+            return Task.FromResult(1);
+        }
+
+        /// <summary>
+        /// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
+        /// </summary>
+        public void Dispose()
+        {
+            var clientToDispose = Interlocked.Exchange(ref this.client, null);
+            if (clientToDispose != null)
+            {
+                try
+                {
+                    clientToDispose.Close();
+                }
+                catch
+                {
+                    // ignored
+                }
+            }
+        }
+    }
+}
